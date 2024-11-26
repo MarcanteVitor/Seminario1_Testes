6ff778b0f6f0bc75a9cbf0446a801eff4bdc69b8
MarcanteVitor <marcanteluizvitor@gmail.com>
2024-11-26 19:46:57 -0300

    removendo

--- index.js
+++ index.js
@@ -1,23 +1,18 @@
 import ProductService from './productService';
 import OrderService from './OrderService'
 
-// Inicializando serviços
 const productService = new ProductService();
 const orderService = new OrderService();
 
-// Criando produtos
 const product1 = productService.createProduct(1, 'Laptop', 1500);
 const product2 = productService.createProduct(2, 'Mouse', 50);
 
-// Aplicando desconto
 product1.applyDiscount(10);
 
-// Criando um pedido
 const order1 = orderService.createOrder(1, 'Customer123');
 order1.addProduct(product1);
 order1.addProduct(product2);
 
-// Exibindo informações
 console.log('Produtos:', productService.getAllProducts());
 console.log('Pedidos:', orderService.getAllOrders());
 console.log('Total do Pedido:', order1.calculateTotal());
